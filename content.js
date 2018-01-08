4d34e061117d71529e7958c5daeb1da9e0ba1f4b
Paul Irish <paul.irish@gmail.com>
2018-01-08 13:02:26 -0800

    inspectable injected stylesheets

--- content.js
+++ content.js
@@ -2,3 +2,15 @@ var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
+
+
+function injectStyles(url) {
+  var elem = document.createElement('link');
+  elem.rel = 'stylesheet';
+  elem.setAttribute('href', url);
+  document.body.appendChild(elem);
+}
+
+injectStyles(chrome.extension.getURL('styles.css'));
+
+
